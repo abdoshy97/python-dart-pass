c64d95c509421d6dc7503fb6c5fa99b3099a0578
abdoshy97 <45370310+abdoshy97@users.noreply.github.com>
2022-04-12 10:42:47 +0300

    Add files via upload
    the first Q

---
+++ dart-pass/dart.dart
@@ -0,0 +1,5 @@
+main() {
+  const hour = DateTime.now().hour;
+  print(hour);
+}
+//Because we used const Which makes the variable a constant value and time is a variable value and to solve this problem we use ( var)
